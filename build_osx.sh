f5e1775d0047e14f9fd8172dd49bc66ce210b491
Maurits Lamers <maurits@weidestraat.nl>
2013-06-01 18:15:37 +0200

    working compilation for node v0.10

--- build_osx.sh
+++ build_osx.sh
@@ -26,68 +26,102 @@ mkdir -p tmp
 mkdir -p build
 mkdir -p out
 
-#echo PWD is $BUILDPWD
-#echo Downloading and compiling pixman to $TMPDIR/pixman.tar.gz
-#curl http://www.cairographics.org/releases/pixman-$VERSION_PIXMAN.tar.gz -z $TMPDIR/pixman.tar.gz -o $TMPDIR/pixman.tar.gz
-#cd $BUILDDIR
-#tar -xvzf $TMPDIR/pixman.tar.gz && cd $BUILDDIR/pixman-$VERSION_PIXMAN/
-#echo ./configure --prefix=$BUILDDIR --disable-dependency-tracking
-#./configure --prefix=$OUTDIR --disable-dependency-tracking
-#make install 
-#cd $BUILDPWD
-#
-#echo Downloading and compiling libpng to $TMPDIR/libpng.tar.gz
-#curl -L http://sourceforge.net/projects/libpng/files/libpng$VERSION_LIBPNGMAIN/$VERSION_LIBPNG/libpng-$VERSION_LIBPNG.tar.gz/download -z $TMPDIR/libpng.tar.gz -o $TMPDIR/libpng.tar.gz
-#cd $BUILDDIR
-#tar -xvzf $TMPDIR/libpng.tar.gz && cd $BUILDDIR/libpng-$VERSION_LIBPNG
-#echo ./configure --prefix=$OUTDIR --disable-dependency-tracking
-#./configure --prefix=$OUTDIR --disable-dependency-tracking
-#make install
-#cd $BUILDDIR
+echo PWD is $BUILDPWD
+echo Downloading and compiling pixman to $TMPDIR/pixman.tar.gz
+curl http://www.cairographics.org/releases/pixman-$VERSION_PIXMAN.tar.gz -z $TMPDIR/pixman.tar.gz -o $TMPDIR/pixman.tar.gz
+cd $BUILDDIR
+tar -xvzf $TMPDIR/pixman.tar.gz && cd $BUILDDIR/pixman-$VERSION_PIXMAN/
+echo ./configure --prefix=$BUILDDIR --disable-dependency-tracking
+./configure --prefix=$OUTDIR --disable-dependency-tracking
+make install 
+cd $BUILDPWD
 
-#echo Downloading and compiling libfreetype
-#curl -L http://download.savannah.gnu.org/releases/freetype/freetype-$VERSION_FREETYPE.tar.gz -z $TMPDIR/freetype.tar.gz -o $TMPDIR/freetype.tar.gz
-#cd $BUILDDIR
-#tar -xvzf $TMPDIR/freetype.tar.gz
-#cd $BUILDDIR/freetype-$VERSION_FREETYPE
-#PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
-#make install
-#cd $BUILDDIR
-#
-#echo Downloading and compiling fontconfig
-#curl -L http://fontconfig.org/release/fontconfig-$VERSION_FONTCONFIG.tar.gz -z $TMPDIR/fontconfig.tar.gz -o $TMPDIR/fontconfig.tar.gz
-#cd $BUILDDIR
-#tar -xvzf $TMPDIR/fontconfig.tar.gz
-#cd $BUILDDIR/fontconfig-$VERSION_FONTCONFIG
-#PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
-#make install
-#cd $BUILDDIR
+echo Downloading and compiling libpng to $TMPDIR/libpng.tar.gz
+curl -L http://sourceforge.net/projects/libpng/files/libpng$VERSION_LIBPNGMAIN/$VERSION_LIBPNG/libpng-$VERSION_LIBPNG.tar.gz/download -z $TMPDIR/libpng.tar.gz -o $TMPDIR/libpng.tar.gz
+cd $BUILDDIR
+tar -xvzf $TMPDIR/libpng.tar.gz && cd $BUILDDIR/libpng-$VERSION_LIBPNG
+echo ./configure --prefix=$OUTDIR --disable-dependency-tracking
+./configure --prefix=$OUTDIR --disable-dependency-tracking
+make install
+cd $BUILDDIR
 
-#echo Downloading and compiling cairo
-#if [ -a $TMPDIR/cairo.tar -ne true ] 
-#then
-#  curl http://www.cairographics.org/releases/cairo-$VERSION_CAIRO.tar.xz -z $TMPDIR/cairo.tar.xz -o $TMPDIR/cairo.tar.xz
-#  xz -d $TMPDIR/cairo.tar.xz
-#fi
-#cd $BUILDDIR
-#tar -xvf $TMPDIR/cairo.tar 
-#cd $BUILDDIR/cairo-$VERSION_CAIRO
-#echo ./configure --prefix=$OUTDIR --disable-dependency-tracking
-#PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
-#make install
-#cd $BUILDDIR
+echo Downloading and compiling libfreetype
+curl -L http://download.savannah.gnu.org/releases/freetype/freetype-$VERSION_FREETYPE.tar.gz -z $TMPDIR/freetype.tar.gz -o $TMPDIR/freetype.tar.gz
+cd $BUILDDIR
+tar -xvzf $TMPDIR/freetype.tar.gz
+cd $BUILDDIR/freetype-$VERSION_FREETYPE
+PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
+make install
+cd $BUILDDIR
+
+echo Downloading and compiling fontconfig
+curl -L http://fontconfig.org/release/fontconfig-$VERSION_FONTCONFIG.tar.gz -z $TMPDIR/fontconfig.tar.gz -o $TMPDIR/fontconfig.tar.gz
+cd $BUILDDIR
+if[ -a $BUILDDIR/fontconfig-$VERSION_FONTCONFIG ] #recompile fontconfig by default to prevent fcache to run
+then
+  rm -r $BUILDDIR/fontconfig-$VERSION_FONTCONFIG
+fi
+tar -xvzf $TMPDIR/fontconfig.tar.gz
+cd $BUILDDIR/fontconfig-$VERSION_FONTCONFIG
+PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
+make install
+cd $BUILDDIR
+
+echo Downloading and compiling cairo
+if [ -a $TMPDIR/cairo.tar -ne true ] 
+then
+  curl http://www.cairographics.org/releases/cairo-$VERSION_CAIRO.tar.xz -z $TMPDIR/cairo.tar.xz -o $TMPDIR/cairo.tar.xz
+  xz -d $TMPDIR/cairo.tar.xz
+fi
+cd $BUILDDIR
+tar -xvf $TMPDIR/cairo.tar 
+cd $BUILDDIR/cairo-$VERSION_CAIRO
+echo ./configure --prefix=$OUTDIR --disable-dependency-tracking
+PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
+make install
+cd $BUILDDIR
+
+
+
+## because node v0.10 doesn't allow module renaming _after_ the fact
+## doing a simple npm install is not good enough anymore,
+## we have to change the source code to get the right module file and name :##@$#$%#$%#$
 
 #PKG_CONFIG_PATH=$PKG_CONFIG_PATH npm install canvas
 
+if [ ! -d $BUILDPWD/node-canvas ] 
+then
+  git clone git://github.com/LearnBoost/node-canvas  
+else 
+  cd node-canvas
+  git checkout -f master
+  cd ..
+fi
+
+cd node-canvas
+mv src/init.cc src/initcc.old
+sed s/NODE_MODULE\(canvas,init\)/NODE_MODULE\(canvas_osx,init\)/ < src/initcc.old  > src/init.cc
+chmod 755 src/init.cc
+mv binding.gyp bindinggyp.old
+sed s/canvas/canvas_osx/ < bindinggyp.old > binding.gyp
+PKG_CONFIG_PATH=$PKG_CONFIG_PATH node-gyp rebuild
+
 ## Now we are going to create the node-canvas-bin package
-## first check out the current one, then copy the new files over
-#git clone git@github.com:mauritslamers/node-canvas-bin
+## first check out the current one (if it doesn't exist yet), then copy the new files over
+if [ ! -d $NODEMODULEDIR ]
+then
+  git clone git://github.com/mauritslamers/node-canvas-bin
+else 
+  cd $NODEMODULEDIR
+  #git checkout -f master
+  cd $BUILDPWD
+fi
 
-## we copy carefully
-
-cp -r node_modules/canvas/lib/* $NODEMODULEDIR/lib
+# we copy carefully
+cd $BUILDPWD
+cp -r node-canvas/lib/* $NODEMODULEDIR/lib
 mkdir -p $NODEMODULEDIR/build/Release
-cp node_modules/canvas/build/Release/canvas.node $NODEMODULEDIR/build/Release/canvas.osx.node
+cp node-canvas/build/Release/canvas_osx.node $NODEMODULEDIR/build/Release/canvas_osx.node
 
 cd $NODEMODULEDIR
 ## but we don't want to overwrite the bindings file
@@ -118,8 +152,8 @@ install_name_tool -change $OUTDIR/lib/libpng16.16.dylib @loader_path/libpng16.16
 cd ..
 
 #canvas.node
-install_name_tool -change $OUTDIR/lib/libpixman-1.0.dylib @loader_path/../../binlibs/libpixman-1.0.dylib build/Release/canvas.osx.node
-install_name_tool -change $OUTDIR/lib/libcairo.2.dylib @loader_path/../../binlibs/libcairo.2.dylib build/Release/canvas.osx.node
+install_name_tool -change $OUTDIR/lib/libpixman-1.0.dylib @loader_path/../../binlibs/libpixman-1.0.dylib build/Release/canvas_osx.node
+install_name_tool -change $OUTDIR/lib/libcairo.2.dylib @loader_path/../../binlibs/libcairo.2.dylib build/Release/canvas_osx.node
 
 #done?
 echo Done copying and installing relative paths. Don\'t forget to commit the changes.
