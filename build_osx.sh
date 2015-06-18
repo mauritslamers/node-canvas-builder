3d2d1b12cbc578213c27d2cd700e69856c9e0fad
Maurits Lamers <maurits@weidestraat.nl>
2015-06-18 15:40:09 +0200

    updating for newer layout for node-canvas-bin-libs

--- build_osx.sh
+++ build_osx.sh
@@ -11,7 +11,7 @@ BUILDPWD=`pwd`
 BUILDDIR=$BUILDPWD/build
 TMPDIR=$BUILDPWD/tmp
 OUTDIR=$BUILDPWD/out
-NODEMODULEDIR=$BUILDPWD/node-canvas-bin
+NODEMODULEDIR=$BUILDPWD/node-canvas-bin-libs
 
 PKG_CONFIG_PATH=$OUTDIR/lib/pkgconfig
 
@@ -23,6 +23,8 @@ VERSION_LIBPNG=1.5.16
 VERSION_CAIRO=1.10.2
 VERSION_FREETYPE=2.4.11
 VERSION_FONTCONFIG=2.10.93
+NODEVERSION=`node -v | sed 's/v\([0-9]*\.[0-9]*\).[0-9]/\1/'`
+NODECANVASBRANCH=osx_"$NODEVERSION"
 
 mkdir -p tmp
 mkdir -p build
@@ -75,52 +77,51 @@ cd $BUILDDIR
 tar -xvzf $TMPDIR/pixman.tar.gz && cd $BUILDDIR/pixman-$VERSION_PIXMAN/
 echo ./configure --prefix=$BUILDDIR --disable-dependency-tracking
 PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
-make install 
+make install
 cd $BUILDPWD
 
 echo Downloading and compiling cairo
-if [ ! -a $TMPDIR/cairo.tar ] 
+if [ ! -a $TMPDIR/cairo.tar ]
 then
   #curl http://www.cairographics.org/releases/cairo-$VERSION_CAIRO.tar.xz -z $TMPDIR/cairo.tar.xz -o $TMPDIR/cairo.tar.xz
   #xz -d $TMPDIR/cairo.tar.xz
   curl http://www.cairographics.org/releases/cairo-$VERSION_CAIRO.tar.gz -z $TMPDIR/cairo.tar.gz -o $TMPDIR/cairo.tar.gz
-  
+
 fi
 cd $BUILDDIR
-#tar -xvf $TMPDIR/cairo.tar 
+#tar -xvf $TMPDIR/cairo.tar
 tar -xvzf $TMPDIR/cairo.tar.gz
 cd $BUILDDIR/cairo-$VERSION_CAIRO
-echo ./configure --prefix=$OUTDIR --disable-dependency-tracking
-PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking
+echo ./configure --prefix=$OUTDIR --disable-dependency-tracking --disable-xlib --disable-xlib-xrender --disable-xcb --disable-xlib-xcb --disable-xcb-shm
+PKG_CONFIG_PATH=$PKG_CONFIG_PATH ./configure --prefix=$OUTDIR --disable-dependency-tracking --disable-xlib --disable-xlib-xrender --disable-xcb --disable-xlib-xcb --disable-xcb-shm
 make install
 cd $BUILDDIR
 
 
-## because node v0.10 doesn't allow module renaming _after_ the fact
+## because from node v0.10 it isn't allowed to rename a module _after_ the fact
 ## doing a simple npm install is not good enough anymore,
-## we have to change the source code to get the right module file and name :##@$#$%#$%#$
 
 #PKG_CONFIG_PATH=$PKG_CONFIG_PATH npm install canvas
 
-if [ ! -d $BUILDPWD/node-canvas ] 
+if [ ! -d $BUILDPWD/node-canvas ]
 then
-  git clone git://github.com/LearnBoost/node-canvas  
-else 
+  git clone git://github.com/LearnBoost/node-canvas
+else
   cd $BUILDPWD/node-canvas
   git checkout -f master
-  if [ -a ./src/initcc.old ] 
-  then 
+  if [ -a ./src/initcc.old ]
+  then
     rm ./src/initcc.old
   fi
-  if [ -a ./bindinggyp.old ] 
-  then 
-    rm ./bindinggyp.old 
+  if [ -a ./bindinggyp.old ]
+  then
+    rm ./bindinggyp.old
   fi
   cd ..
 fi
 
 cd $BUILDPWD/node-canvas
-if [ -a ./src/initcc.old ] 
+if [ -a ./src/initcc.old ]
 then
   rm ./src/initcc.old
 fi
@@ -135,43 +136,55 @@ mv binding.gyp bindinggyp.old
 sed s/canvas/canvas_osx/ < bindinggyp.old > binding.gyp
 PKG_CONFIG_PATH=$PKG_CONFIG_PATH node-gyp rebuild
 
+cd $BUILDPWD
 ## Now we are going to create the node-canvas-bin package
-## first check out the current one (if it doesn't exist yet), then copy the new files over
+## first check out the current one, then copy the new files over
 if [ ! -d $NODEMODULEDIR ]
 then
-  git clone git://github.com/mauritslamers/node-canvas-bin
-else 
-  cd $NODEMODULEDIR
-  git checkout -f node-v0.10
-  cd $BUILDPWD
+  git clone git@github.com:mauritslamers/node-canvas-bin-libs
+fi
+
+cd $NODEMODULEDIR
+if [ ! `git checkout -f $NODECANVASBRANCH` ]
+  then
+    # do a check whether we are actually already in the correct branch, which can happen when a new branch is created.
+    if [[ `git status -b --porcelain` != *$NODECANVASBRANCH* ]]
+      then
+        echo The branch you are trying to check out \($NODECANVASBRANCH\) doesn\'t exist yet.
+        echo Create it first by going into the node-canvas-bin-libs directry and execute git checkout --orphan $NODECANVASBRANCH.
+        echo Then remove all the content using git rm -rf.
+        exit 1;
+    fi
+    #else, we are in the correct branch, just continue
 fi
 
 # we copy carefully
 cd $BUILDPWD
-cp -r node-canvas/lib/* $NODEMODULEDIR/lib
-mkdir -p $NODEMODULEDIR/build/Release
-cp $BUILDPWD/node-canvas/build/Release/canvas_osx.node $NODEMODULEDIR/build/Release/canvas_osx.node
+#cp -r node-canvas/lib/* $NODEMODULEDIR/lib
+mkdir -p $NODEMODULEDIR/binlib
+cp $BUILDPWD/node-canvas/build/Release/canvas_osx.node $NODEMODULEDIR/binlib/canvas_osx.node
 
-cd $NODEMODULEDIR
+#cd $NODEMODULEDIR
 ## but we don't want to overwrite the bindings file
-git checkout $NODEMODULEDIR/lib/bindings.js
+#git checkout $NODEMODULEDIR/lib/bindings.js
 
-mkdir -p $NODEMODULEDIR/binlibs
-cd $NODEMODULEDIR/binlibs
+#mkdir -p $NODEMODULEDIR/binlib
+cd $NODEMODULEDIR/binlib
 
-cp $OUTDIR/lib/libpixman-1.* $NODEMODULEDIR/binlibs
-cp $OUTDIR/lib/libcairo.* $NODEMODULEDIR/binlibs
-cp $OUTDIR/lib/libfreetype.* $NODEMODULEDIR/binlibs
-cp $OUTDIR/lib/libpng15.* $NODEMODULEDIR/binlibs
-cp $OUTDIR/lib/libjpeg.* $NODEMODULEDIR/binlibs
-cp $OUTDIR/lib/libfontconfig.* $NODEMODULEDIR/binlibs
+cp $OUTDIR/lib/libpixman-1.0.dylib $NODEMODULEDIR/binlib
+cp $OUTDIR/lib/libcairo.dylib $NODEMODULEDIR/binlib
+cp $OUTDIR/lib/libcairo.2.dylib $NODEMODULEDIR/binlib
+cp $OUTDIR/lib/libfreetype.6.dylib $NODEMODULEDIR/binlib
+cp $OUTDIR/lib/libpng15.15.dylib $NODEMODULEDIR/binlib
+cp $OUTDIR/lib/libjpeg.8.dylib $NODEMODULEDIR/binlib
+cp $OUTDIR/lib/libfontconfig.1.dylib $NODEMODULEDIR/binlib
 
 ##start renaming, pixman first
-install_name_tool -change $OUTDIR/lib/libpixman-1.0.dylib ./binlibs/libpixman-1.0.dylib libpixman-1.0.dylib
+install_name_tool -change $OUTDIR/lib/libpixman-1.0.dylib @loader_path/libpixman-1.0.dylib libpixman-1.0.dylib
 
 #libfontconfig
 install_name_tool -change $OUTDIR/lib/libfreetype.6.dylib @loader_path/libfreetype.6.dylib libfontconfig.dylib
-install_name_tool -change $OUTDIR/lib/libfreetype.6.dylib @loader_path/libfreetype.6.dylib libfontconfig.1.dylib
+#install_name_tool -change $OUTDIR/lib/libfreetype.6.dylib @loader_path/libfreetype.6.dylib libfontconfig.1.dylib
 
 ##cairo
 install_name_tool -change $OUTDIR/lib/libcairo.2.dylib @loader_path/libcairo.2.dylib libcairo.dylib
@@ -189,9 +202,13 @@ install_name_tool -change $OUTDIR/lib/libfontconfig.1.dylib @loader_path/libfont
 cd ..
 
 #canvas.node
-install_name_tool -change $OUTDIR/lib/libpixman-1.0.dylib @loader_path/../../binlibs/libpixman-1.0.dylib build/Release/canvas_osx.node
-install_name_tool -change $OUTDIR/lib/libcairo.2.dylib @loader_path/../../binlibs/libcairo.2.dylib build/Release/canvas_osx.node
-install_name_tool -change $OUTDIR/lib/libjpeg.8.dylib @loader_path/../../binlibs/libjpeg.8.dylib build/Release/canvas_osx.node
+install_name_tool -change $OUTDIR/lib/libpixman-1.0.dylib @loader_path/libpixman-1.0.dylib canvas_osx.node
+install_name_tool -change $OUTDIR/lib/libcairo.2.dylib @loader_path/libcairo.2.dylib canvas_osx.node
+install_name_tool -change $OUTDIR/lib/libjpeg.8.dylib @loader_path/libjpeg.8.dylib canvas_osx.node
+
+#Before testing, run the package.json creator
+cd $BUILDPWD
+node create_packagejson.js
 
 #we temporarily rename the out folder, so any problems will be visible on the test
 mv $BUILDPWD/out $BUILDPWD/out_tmp
