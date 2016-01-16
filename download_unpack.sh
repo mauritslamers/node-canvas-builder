78679e3e634d3c41dab5093cb20a1734728b5b6d
Maurits Lamers <maurits@weidestraat.nl>
2016-01-16 17:46:00 +0100

    fix unpack command of cairo

--- download_unpack.sh
+++ download_unpack.sh
@@ -25,6 +25,6 @@ tar -xvzf ../download/libfontconfig.tar.gz
 tar -xvzf ../download/libjpeg.tar.gz
 tar -xvzf ../download/libpng.tar.gz
 tar -xvzf ../download/giflib.tar.gz
-xz -d ../download/cairo.tar.xz
+tar -xzJf ../download/cairo.tar.xz
 
 cd ..
